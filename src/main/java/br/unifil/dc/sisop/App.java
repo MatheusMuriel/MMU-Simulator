a93f40ceb0806475be89fee1974bc0185eee9a42
Matheus Muriel <matheus.muriel@outlook.com>
2018-11-19 20:30:06 -0200

    Refeito o projeto IntelliJ

---
+++ src/main/java/br/unifil/dc/sisop/App.java
@@ -0,0 +1,13 @@
+package br.unifil.dc.sisop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
