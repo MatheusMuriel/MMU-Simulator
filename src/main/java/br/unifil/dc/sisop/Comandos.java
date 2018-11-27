b3dc6010ef2461127d0246c368ee2b348acca80d
Matheus Muriel <matheus.muriel@outlook.com>
2018-11-27 09:11:13 -0200

    Update Manhani
    Alterações mandadas pelo Manhani

--- src/main/java/br/unifil/dc/sisop/Comandos.java
+++ src/main/java/br/unifil/dc/sisop/Comandos.java
@@ -3,6 +3,7 @@ package br.unifil.dc.sisop;
 import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.Scanner;
 
 import static br.unifil.dc.sisop.Terminal.Simulacao.memoriaInstalada;
 import static br.unifil.dc.sisop.Terminal.Simulacao.tipoSimulacao;
@@ -83,7 +84,7 @@ public class Comandos {
      * @param comando
      */
     public void chamaComando(Terminal.Comando comando) {
-        System.out.println("Metodo Comandos.chama comando ainda não implementado.");
+        System.out.println("Metodo Comandos.chamaComando ainda não implementado.");
 
     }
 
@@ -109,57 +110,8 @@ public class Comandos {
                 return false;
 
             case ("configurar"):
-                int quantBits;
-                TiposComandos comando;
-                if ((quantBits = ma.pegaNumero(parametros.get(0))) == -1) {
-                    System.err.println("Quantidade de Bits invalida ou inexistente.");
-                    return false;
-                }
-
-                //Capturar a entrada de Inteiro e haxadecimal aqui
-
-                switch (parametros.get(2)){
-                    case ("registradores-base-e-limite"):
-                        switch (parametros.get(3)){
-                            case ("bitmap"):
-                                int t; //Tamanho de cada pedaço de alocação de memoria.
-                                //Optional<Integer> op = ma.pegaNumero(parametros.get(3));
-                                if ((t = ma.pegaNumero(parametros.get(4))) == -1) {
-                                    System.err.println("Quantidade de Bits do bitmap invalida ou inexistente.");
-                                    return false;
-                                } else {
-                                    //CHEGADA FINAL
-                                    comando = TiposComandos.RegistradoresBitmap;
-                                    //System.out.println("bitmap com tamanho " + t);
-                                    tipoSimulacao = comando;
-                                    return true;
-                                }
-                            case ("lista-encadeada"):
-                                comando = TiposComandos.RegistradoresLista;
-                                //System.out.println("Lista encadeada");
-                                tipoSimulacao = comando;
-                                return true;
-                        }
-                    case ("memoria-virtual"):
-                        int qBitsPosMSB;
-
-                        if ((qBitsPosMSB = ma.pegaNumero(parametros.get(3))) == -1){
-                            System.err.println("Quantidade de Bits invalida ou inexistente.");
-                            System.err.println("Digite a quantidade de bits a partir do MSB para a indexação de páginas de um processo.");
-
-                            return false;
-                        }else {
-                            comando = TiposComandos.Memoria;
-                            //System.out.println("memoria virtual com quantidade de bits " + qBitsPosMSB);
-                            tipoSimulacao = comando;
-                            return true;
-                        }
-                    default:
-                        System.err.println("Especifique o tipo de MMU (Sem espaços ou acentos e separado por traço)");
-                        return false;
-                }
-
-                //Fazer a checagem de logica entre os parametros aqui
+                tamanhoDaMemoria();
+                return true;
 
             case ("processo"):
                 //Precisa receber o PID do novo processo
@@ -208,12 +160,134 @@ public class Comandos {
 
     /**
      * cria um novo ambiente de simulação de gerenciamento de memória
+     * Esse metodo é chamado para processar um entrada com os parametros já passados.
      *
-     * @param cmd
+     *Tbm define aqui a variavel publica estatica de qual tipo de simulação se trata
      */
-    public static void configurar(Terminal.Comando cmd){
+    public static boolean configurar(Terminal.Comando cmd, ArrayList<String> parametros){
+        MetodosAuxiliares ma = new MetodosAuxiliares();
+        int quantBits;
+        TiposComandos comando;
+        if ((quantBits = ma.pegaNumero(parametros.get(0))) == -1) {
+            System.err.println("Quantidade de Bits invalida ou inexistente.");
+            return false;
+        }
+
+        //Capturar a entrada de Inteiro e haxadecimal aqui
+
+        switch (parametros.get(2)){
+            case ("registradores-base-e-limite"):
+                switch (parametros.get(3)){
+                    case ("bitmap"):
+                        int t; //Tamanho de cada pedaço de alocação de memoria.
+                        //Optional<Integer> op = ma.pegaNumero(parametros.get(3));
+                        if ((t = ma.pegaNumero(parametros.get(4))) == -1) {
+                            System.err.println("Quantidade de Bits do bitmap invalida ou inexistente.");
+                            return false;
+                        } else {
+                            //CHEGADA FINAL
+                            comando = TiposComandos.RegistradoresBitmap;
+                            //System.out.println("bitmap com tamanho " + t);
+                            tipoSimulacao = comando;
+                            return true;
+                        }
+                    case ("lista-encadeada"):
+                        comando = TiposComandos.RegistradoresLista;
+                        //System.out.println("Lista encadeada");
+                        tipoSimulacao = comando;
+                        return true;
+                }
+            case ("memoria-virtual"):
+                int qBitsPosMSB;
+
+                if ((qBitsPosMSB = ma.pegaNumero(parametros.get(3))) == -1){
+                    System.err.println("Quantidade de Bits invalida ou inexistente.");
+                    System.err.println("Digite a quantidade de bits a partir do MSB para a indexação de páginas de um processo.");
+
+                    return false;
+                }else {
+                    comando = TiposComandos.Memoria;
+                    //System.out.println("memoria virtual com quantidade de bits " + qBitsPosMSB);
+                    tipoSimulacao = comando;
+                    return true;
+                }
+            default:
+                System.err.println("Especifique o tipo de MMU (Sem espaços ou acentos e separado por traço)");
+                return false;
+        }
+
+        //Fazer a checagem de logica entre os parametros aqui
+
 
     }
+
+    /*Processamento da entrada com apenas o comando configurar (Sem parametros) */
+    private boolean tamanhoDaMemoria() {
+        int memoriaReal;
+        String procurarPor = "0x";
+        Scanner bits = new Scanner(System.in);
+        int numBits;
+        System.out.println("Digite os bits da memoria");
+        numBits = bits.nextInt();
+        Scanner entradaMemoriaFisica = new Scanner(System.in);
+        String memoriaFisica;
+        System.out.println("Digite o tamanho da memoria fisica");
+        memoriaFisica = entradaMemoriaFisica.nextLine();
+        if(memoriaFisica.contains(procurarPor)){
+            String novaString = memoriaFisica.replace("x", "");
+            memoriaReal = converteHex(novaString);
+        }else{
+            memoriaReal = Integer.parseInt(memoriaFisica);
+        }
+
+
+        verificarValidadeDaMemoria(numBits, memoriaReal);
+        System.out.println();
+        System.out.println(numBits);
+        System.out.println(memoriaFisica);
+        System.out.println(memoriaReal);
+
+        return false;
+    }
+
+    private void verificarValidadeDaMemoria(int numBits, int memoriaReal) {
+        int quantidadeQuadros = 1;
+        int contador = 1;
+        while (quantidadeQuadros < memoriaReal){
+            quantidadeQuadros = quantidadeQuadros*2;
+            contador++;
+        }
+
+        if(quantidadeQuadros == memoriaReal){
+            criaMemoria(numBits, contador);
+        }else{
+            System.out.println("entrada invalida! você precisa digitar uma memoria valida");
+        }
+    }
+
+    private void criaMemoria(int numBits, int contador) {
+        int[] arryMemoria = new int[contador];
+        int quantBits = numBits;
+        for (int i = 0; i < arryMemoria.length-1; i++){
+            arryMemoria[i]= numBits;
+            numBits = numBits + quantBits;
+            System.out.print(arryMemoria[i]+ ",");
+        }
+    }
+
+    private int converteHex(String memoriaFisica) {
+        String digits = "0123456789ABCDEF";
+        memoriaFisica = memoriaFisica.toUpperCase();
+        int val = 0;
+        for (int i = 0; i < memoriaFisica.length(); i++) {
+            char c = memoriaFisica.charAt(i);
+            int d = digits.indexOf(c);
+            val = 16*val + d;
+        }
+        return val;
+    }
+    /* */
+
 
     public static void processo(Terminal.Comando cmd){
 
